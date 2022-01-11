4026b70a875cab7bb861903aa9b5f521f71159f0
Tobias Blaksley <tobias.blaksleyiraola@cordialfinanciera.com.ar>
2022-01-11 01:24:04 -0300

    cree el header de la app

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderTitleComponent } from './header-title/header-title.component';
+
+
+
+@NgModule({
+  declarations: [HeaderTitleComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [HeaderTitleComponent]
+})
+export class SharedModule { }
